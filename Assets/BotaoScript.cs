effe54eefc48124a2f808867b8c4ff2db8080384
Luis4raujo <luisaraujo.ifba@gmail>
2015-09-20 00:27:54 -0300

    Implementanção Tela Inicial e Menu
    Implementação da Tela inicial como os animations das logos e passagen de
    nível como fadein. Implementação do Menu como Design Patter Observer
    para botões.

---
+++ Assets/BotaoScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BotaoScript : MonoBehaviour, IObservador {
+
+	public Sprite spriteSelect;
+	public Sprite sprite;
+	private bool ativo;
+	public int identificador;
+
+	// Use this for initialization
+	void Start () {
+		ativo = false;
+		GetComponent<SpriteRenderer> ().sprite = sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((ativo) && (GetComponent<SpriteRenderer> ().sprite != spriteSelect)) {
+			GetComponent<SpriteRenderer> ().sprite = spriteSelect;
+			Debug.Log ("ok "+identificador);
+		} else if ((!ativo) && (GetComponent<SpriteRenderer> ().sprite != sprite)) {
+			GetComponent<SpriteRenderer> ().sprite = sprite;
+		}
+	
+	}
+
+
+	public void atualizar(IObservavel obj){
+
+		if (obj.getEstado () == this.identificador) {
+			ativo = true;
+		} else if(ativo == true) {
+			ativo = false;
+		}
+
+	}
+ 
+}
---
+++ Assets/ControllerInicio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInicio : ControllerPAI {
+
+	public GameObject logo;
+	private Animator aniLogo;
+
+	// Use this for initialization
+	void Start () {
+	
+		aniLogo = logo.GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if( aniLogo.GetCurrentAnimatorStateInfo (0).IsName("LogoGRUDE_out") ){
+			GameObject.Find("ControllerCenaInicio").GetComponent<ControllerCenaInicio>().NextLevel(1, false);
+		}
+	}
+}
